5a0f9c41971aa8a40fc745a12c1bf6662d47c513
Ruslan Yakhin <Rusl.215k58@gmail.com>
2019-11-03 19:00:49 +0500

    Documented DateUtil class

--- app/src/main/java/army/org/dmbtimer/utils/DateUtil.kt
+++ app/src/main/java/army/org/dmbtimer/utils/DateUtil.kt
@@ -4,6 +4,9 @@ import org.joda.time.*
 import java.util.*
 
 
+/**
+ * Утилитный класс для вычисления времени
+ */
 class DateUtil(timeInMills: Long) {
 
     private val startDate: LocalDateTime
@@ -19,17 +22,38 @@ class DateUtil(timeInMills: Long) {
 
     }
 
+    /**
+     * @return - Количество прошедших секунд с начала призыва до текущей даты
+     */
     fun passedTimeInSec(): Seconds = Seconds.secondsBetween(startDate, nowDate)
+
+    /**
+     * @return - Количество прошедших секунд с начала призыва до ДМБ
+     */
     fun totalTimeInSec(): Seconds = Seconds.secondsBetween(startDate, finisDate)
 
+    /**
+     * @return - Количество прошедших дней с начала призыва до текущей даты
+     */
     fun passedDays(): Days = Days.daysBetween(startDate, nowDate)
+
+    /**
+     * @return - Количество прошедших часов с начала призыва до текущей даты с учетом дней
+     */
     fun passedHours(): Hours = Hours.hoursBetween(startDate, nowDate).minus(passedDays().days * 24)
+
+    /**
+     * @return - Количество прошедших минут с начала призыва до текущей даты с учетом дней и часов
+     */
     fun passedMinutes(): Minutes =
         Minutes.minutesBetween(
             startDate,
             nowDate
         ).minus(passedDays().days * 24 * 60).minus(passedHours().hours * 60)
 
+    /**
+     * @return - Количество прошедших секунд с начала призыва до текущей даты с учетом дней, часов и минут
+     */
     fun passedSeconds(): Seconds =
         Seconds.secondsBetween(
             startDate,
@@ -38,14 +62,28 @@ class DateUtil(timeInMills: Long) {
             passedMinutes().minutes * 60
         )
 
+    /**
+     * @return - Количество оставшихся дней с текущей даты до ДМБ
+     */
     fun leftDays(): Days = Days.daysBetween(nowDate, finisDate)
+
+    /**
+     * @return - Количество оставшихся часов с текущей даты до ДМБ с учетом дней
+     */
     fun leftHours(): Hours = Hours.hoursBetween(nowDate, finisDate).minus(leftDays().days * 24)
+
+    /**
+     * @return - Количество оставшихся минут с текущей даты до ДМБ с учетом дней и часов
+     */
     fun leftMinutes(): Minutes =
         Minutes.minutesBetween(
             nowDate,
             finisDate
         ).minus(leftDays().days * 24 * 60).minus(leftHours().hours * 60)
 
+    /**
+     * @return - Количество оставшихся секунд с текущей даты до ДМБ с учетом дней, часов и минут
+     */
     fun leftSeconds(): Seconds =
         Seconds.secondsBetween(
             nowDate,
